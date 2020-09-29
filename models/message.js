40c24ac6ac013ab3cb027ea89ab3fd9c4ba47049
PH <delportepolo@hotmail.fr>
2020-09-30 01:07:08 +0200

    Mise en place du register, error : missing parameters

--- models/message.js
+++ models/message.js
@@ -3,30 +3,32 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Message extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.Message.belongsTo(models.User, {
-        foreignKey:{
-          allowNull: false,
-        }
-      })
-    }
-  };
-  Message.init({
+  var Message = sequelize.define('Message', {
     idUSERS: DataTypes.INTEGER,
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     attachment: DataTypes.STRING,
     likes: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Message',
-  });
+  },{
+      classMethods: {
+        associate: function(models) {
+          models.Message.belongsTo(models.User, {
+            foreignKey : {
+              allowNull:false
+            }
+          })
+        }
+      }
+    });
+  // Message.init({
+  //   idUSERS: DataTypes.INTEGER,
+  //   title: DataTypes.STRING,
+  //   content: DataTypes.STRING,
+  //   attachment: DataTypes.STRING,
+  //   likes: DataTypes.INTEGER
+  // }, {
+  //   sequelize,
+  //   modelName: 'Message',
+  // });
   return Message;
 };
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 // Imports
 var express = require('express');
+var bodyParser =  require ('body-parser');
+var apiRouter =  require('./apiRouter').router;
 
 // Instantiate server
 var server = express();
 
+// Body Parser configuration
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+
 // Configure routes 
 server.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Bonjour sur mon servveur</h1>'); // 200 car c'est le code HTTP pour le succès de la requête
+    res.status(200).send('<h1>Bonjour sur mon serveur</h1>'); // 200 car c'est le code HTTP pour le succès de la requête
 });
+
+server.use('/api/', apiRouter);  
 
 // Launch server
 server.listen(8080, function() {
